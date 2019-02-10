978d41a3978ffe9cd268704ba55f3a9f3e9eb997
Tomasz Skrzypek <tomas14@uia.no>
2019-02-10 09:59:48 +0100

    Initial commit

---
+++ src/main/java/home/lessons/spring5recipeapp/controllers/IndexController.java
@@ -0,0 +1,17 @@
+package home.lessons.spring5recipeapp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+
+    @RequestMapping({"","/","/index"})
+    public String getIndexPage(){
+        System.out.println("Some masage");
+        return"index";
+    }
+
+
+}
