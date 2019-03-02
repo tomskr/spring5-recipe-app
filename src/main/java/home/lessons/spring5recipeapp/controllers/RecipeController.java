5d62a4aaf85b9d86c0ac0dd8cce951fbeb402539
Tomasz Skrzypek <tomas14@uia.no>
2019-03-03 00:01:28 +0100

    Repair tests

--- src/main/java/home/lessons/spring5recipeapp/controllers/RecipeController.java
+++ src/main/java/home/lessons/spring5recipeapp/controllers/RecipeController.java
@@ -36,11 +36,17 @@ public class RecipeController {
         return "recipe/recipeform";
     }
 
+    @RequestMapping("recipe/{id}/update")
+    public String updateRecipe(@PathVariable String id, Model model){
+        model.addAttribute("recipe", recipeService.findCommandById(Long.valueOf(id)));
+        return  "recipe/recipeform";
+    }
+
     @PostMapping("recipe")
     public String saveOrUpdate(@ModelAttribute RecipeCommand command){
         RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
 
-        return "redirect:/recipe/show/" + savedCommand.getId();
+        return "redirect:/recipe/" + savedCommand.getId() + "/show";
     }
 
 }
