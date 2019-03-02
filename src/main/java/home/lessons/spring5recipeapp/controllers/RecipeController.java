412a703cd99cb8116cc6e98cd32426fec55374f8
Tomasz Skrzypek <tomas14@uia.no>
2019-03-02 23:37:57 +0100

    Added recipe form

--- src/main/java/home/lessons/spring5recipeapp/controllers/RecipeController.java
+++ src/main/java/home/lessons/spring5recipeapp/controllers/RecipeController.java
@@ -1,11 +1,14 @@
 package home.lessons.spring5recipeapp.controllers;
 
 
+import home.lessons.spring5recipeapp.commands.RecipeCommand;
 import home.lessons.spring5recipeapp.services.RecipeService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Slf4j
@@ -25,4 +28,19 @@ public class RecipeController {
 
         return "recipe/show";
     }
+
+    @RequestMapping("recipe/new")
+    public String newRecipe(Model model){
+        model.addAttribute("recipe", new RecipeCommand());
+
+        return "recipe/recipeform";
+    }
+
+    @PostMapping("recipe")
+    public String saveOrUpdate(@ModelAttribute RecipeCommand command){
+        RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
+
+        return "redirect:/recipe/show/" + savedCommand.getId();
+    }
+
 }
